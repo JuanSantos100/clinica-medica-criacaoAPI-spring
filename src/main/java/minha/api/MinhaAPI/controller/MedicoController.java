734387f7944ea86bbccb541dc10c216b6b99b5ff
JuanSantos100 <juanvictortimaster@gmail.com>
2023-02-28 22:16:53 -0300

    Implementando Resources para persistência de dados + Criação de versionamento de banco de dados com Flyway

--- src/main/java/minha/api/MinhaAPI/controller/MedicoController.java
+++ src/main/java/minha/api/MinhaAPI/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package minha.api.MinhaAPI.controller;
 
+import minha.api.MinhaAPI.endereco.Endereco;
 import minha.api.MinhaAPI.medico.DadosCadastroMedico;
+import minha.api.MinhaAPI.medico.Medico;
+import minha.api.MinhaAPI.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
     public void cadastrarMedico(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
+
     }
 
 
---
+++ src/main/java/minha/api/MinhaAPI/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package minha.api.MinhaAPI.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}

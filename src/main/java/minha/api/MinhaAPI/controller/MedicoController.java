5745e8bfc959ea171fc17ddd8f0949d1e674a920
JuanSantos100 <juanvictortimaster@gmail.com>
2023-03-10 14:26:05 -0300

    Métodos retornando objeto de tipo ResponseEntity - CRUD Médico

--- src/main/java/minha/api/MinhaAPI/controller/MedicoController.java
+++ src/main/java/minha/api/MinhaAPI/controller/MedicoController.java
@@ -3,12 +3,15 @@ package minha.api.MinhaAPI.controller;
 import jakarta.validation.Valid;
 import minha.api.MinhaAPI.endereco.Endereco;
 import minha.api.MinhaAPI.medico.*;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -18,37 +21,61 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
-    @PostMapping
-    @Transactional
+
     /*
     Por ser uma operação de escrita
     prescisamos adicionar a annotation
     @Trnasactional para declararmos que terá uma
     transação no banco
      */
-    public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
 
+        /*
+            Ao realizar a criação de um objeto, preciso passar na resposta 3 coisas:
+                1 - Devolver o cabeçalho Location com a URI
+                2 - No corpo da resposta é necessário devolver o objeto recem criado
+                3 - Devolver o código 201 - Created
+         */
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort={"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new); //Realizando a conversão de um objeto Medico para DadosListagemMedico
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort={"nome"}) Pageable paginacao) {
+        var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new); //Realizando a conversão de um objeto Medico para DadosListagemMedico
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico)); //Criamos um novo DTO porque não é ideal passar uma entidade JPA diretamente no controller, utilizamos DTOs para isso.
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluirMedico(@PathVariable Long id) {
+    public ResponseEntity excluirMedico(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listagemPorId(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 
     }
 

f01ced1f74a599323dc53ea653f8727e0bd06e72
JuanSantos100 <juanvictortimaster@gmail.com>
2023-03-12 02:05:58 -0300

    Realizando tratamento de erros : Erro 404, Erro 400

--- src/main/java/minha/api/MinhaAPI/infra/TratadorDeErros.java
+++ src/main/java/minha/api/MinhaAPI/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package minha.api.MinhaAPI.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,8 +16,24 @@ public class TratadorDeErros {
         tratar o caso.
      */
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity TratarErro404() {
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
+
+

01cbaf5b8237b17b87d3bc78c6fd7e8454df4f93
JuanSantos100 <juanvictortimaster@gmail.com>
2023-03-14 22:02:11 -0300

    UsuarioRepository para acesso ao banco de dados + AutenticacaoService para serviço de autenticação de usuário

---
+++ src/main/java/minha/api/MinhaAPI/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package minha.api.MinhaAPI.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}

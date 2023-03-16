0b991bcf7d3e7f3105add833d418edeb4f28dfad
JuanSantos100 <juanvictortimaster@gmail.com>
2023-03-15 21:04:41 -0300

    Utilização de DTO no token + Utilização de variável de ambiente ou valor default no Secret

--- src/main/java/minha/api/MinhaAPI/controller/AutenticacaoController.java
+++ src/main/java/minha/api/MinhaAPI/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package minha.api.MinhaAPI.controller;
 import jakarta.validation.Valid;
 import minha.api.MinhaAPI.domain.usuario.DadosAutenticacao;
 import minha.api.MinhaAPI.domain.usuario.Usuario;
+import minha.api.MinhaAPI.infra.security.DadosTokenJWT;
 import minha.api.MinhaAPI.infra.security.TokenService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,14 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal())); //Pega o usuário logado
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+
+        //Padronizando a aplicação: Recebendo o token dentro de um DTO
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT)); //Pega o usuário logado
     }
 
 }

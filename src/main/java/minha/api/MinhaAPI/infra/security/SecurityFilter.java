b651454908539ed2227ee2453dc00660013cfa44
JuanSantos100 <juanvictortimaster@gmail.com>
2023-03-20 09:10:14 -0300

    Necessário corrigir o método: getSubject() da classe: TokenService

--- src/main/java/minha/api/MinhaAPI/infra/security/SecurityFilter.java
+++ src/main/java/minha/api/MinhaAPI/infra/security/SecurityFilter.java
@@ -3,6 +3,7 @@ package minha.api.MinhaAPI.infra.security;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,18 +12,23 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService; //Injeção de dependência
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
+        System.out.println("token: " + tokenJWT);
+        var subject = tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
 
-        System.out.println(tokenJWT);
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
+       if (authorizationHeader == null) {
             throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
         }
 
